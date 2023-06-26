e77fac84bdfc122e4ed72c15e5614cb2bdfdcdd0
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2023-06-27 01:51:08 +0200

    last

--- src/js/game.js
+++ src/js/game.js
@@ -138,22 +138,6 @@ export class Game extends Engine {
         });
 
 
-        // Horizontale beweging met de pijltoetsen
-        // character.update = function (engine, delta) {
-        //     Actor.prototype.update.call(this, engine, delta);
-
-        //     if (!character.grounded) {
-        //         // Beweeg naar links met de linkerpijl
-        //         if (engine.input.keyboard.isHeld(Input.Keys.Left)) {
-        //             character.pos.x -= 5; // Pas de bewegingssnelheid aan naar wens
-        //         }
-        //         // Beweeg naar rechts met de rechterpijl
-        //         if (engine.input.keyboard.isHeld(Input.Keys.Right)) {
-        //             character.pos.x += 5; // Pas de bewegingssnelheid aan naar wens
-        //         }
-        //     }
-        // };
-
         this.add(character); // Voeg het personage toe na de achtergrondafbeelding
     }
 
